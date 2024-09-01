9767f4f253f5dd39ba94865d2a99926ad7ce9a4f
delmesia <wendellsmesia@gmail.com>
2024-09-01 18:03:54 +0800

    added migration files

---
+++ Makefile
@@ -0,0 +1,6 @@
+# Makefile for running Go application in /cmd/api
+
+.PHONY: run
+
+run:
+	cd cmd/api && go run .
--- script.sh
+++ script.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 echo ""
-echo "Performing GET request to localhost:4000/v1/healthcheck..."
-curl localhost:4000/v1/healthcheck
+echo "Performing GET request to localhost:4000..."
+curl localhost:4000
 echo ""
 
-echo "Performing GET request to localhost:/4000/v1/movies/123"
-curl localhost:4000/v1/movies/123
-echo ""
